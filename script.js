0eef163fdbc938434e806f9a43f84945fd1f0a45
JADundee <Jadundee@icloud.com>
2023-11-10 11:19:49 -0700

    Import export example

---
+++ script.js
@@ -0,0 +1,6 @@
+const script2 = require("./script2")
+
+const a = script2.largeNumber
+const b = 20
+
+console.log(a + b)
---
+++ script2.js
@@ -0,0 +1,5 @@
+const largeNumber = 350
+
+module.exports = {
+    largeNumber: largeNumber
+}
